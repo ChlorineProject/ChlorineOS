ff4080c2c2021f99836a6ff83f43f2880f0134ac
BuildTools <unconfigured@null.spigotmc.org>
2022-09-19 08:11:11 -0400

    Starting multitasking...

--- src/modules/process.cpp
+++ src/modules/process.cpp
@@ -1,1 +1,7 @@
 #include "./process.hpp"
+
+/*
+ *  Here, we will code multitasking and a thread system for ChlorineOS, but first we need to understand the types of multitasking systems. For example, cooperative
+ *  multitasking is a type of multitasking where a thread is ran, and when it stops using the CPU, another process is called. Preemptive multitasking is a little
+ *  different however, because the processes are equally shared some CPU-usage time.
+ */
