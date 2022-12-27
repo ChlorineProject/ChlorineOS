fe293fc215b16eb135b4e3ae596ab04b81cde27b
Your Name <you@example.com>
2022-12-27 10:21:21 -0500

    Removed warnings...

--- source/main.c
+++ source/main.c
@@ -18,6 +18,8 @@
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include <stdint.h>
+
 /*
  *  We'll need the serial driver here, because we will initialize the serial module.
  */
@@ -36,6 +38,16 @@
 #if ARCHITECTURE == 1
 #include "./arch/i386/8259pic.h"
 #endif
+/*
+ *  Let's include `heap.h` to include the heap module...
+ */
+#include "./modules/heap.h"
+
+/*
+ *  Here are the markers that state the start and the end of the kernel...
+ */
+extern char _start;
+extern char _end;
 
 /*
  *  This is the function that is called by `bootstrap.S`. The file `bootstrap.S` gets loaded by the GRUB bootloader, and
@@ -56,4 +68,15 @@ void main()
     printf("Initializing 8259 PIC...\n");
     init_8259pic();
     #endif
+
+    /*
+     *  Let's initialize the allocator...
+     *
+     *  Later, we'll find a way to get the address of the end of physical memory so that we can be able
+     *  to allocate as much memory as possible. For now, only 1GB of RAM is accessible...
+     */
+    init_heap(&_end, &_end + 1073741824);
+    void * pointer0 = kmalloc(1024);
+    void * pointer1 = kmalloc(1024);
+    printf("Allocated 1024 bytes of memory. The start address of this memory is: %d\nAllocated 1024 more bytes of memory.\nThe start address of this new allocated memory is: %d\n", &pointer0, &pointer1);
 }
